a9c781306c34273b3e0890cac2ce71df92089789
jkingz <camilenulla28@gmail.com>
2018-10-11 16:41:10 +0800

    Update Sign Up

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('home.html')
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form':form})
